b6a2ca420e620a689afd4e009067e4a7fd12b123
Nik Nyby <nnyby@columbia.edu>
2017-10-27 15:18:04 -0400

    Use small buttons

--- src/GraphPicker.js
+++ src/GraphPicker.js
@@ -16,56 +16,56 @@ export default class GraphPicker extends React.Component {
                     <JXGBoard id={'jxg-demand-supply'} gType={0} />
                     <button
                          ref={(b1) => { this.b1 = b1; }}
-                         type="button" className="btn btn-default"
+                         type="button" className="btn btn-default btn-sm"
                          onClick={this.handleClick1.bind(this)}>Select</button>
                 </div>
                 <div className="graph-card">
                     Labor Market
                     <JXGBoard id={'jxg-labor-market'} gType={1} />
                     <button
-                         type="button" className="btn btn-default"
+                         type="button" className="btn btn-default btn-sm"
                          onClick={this.handleClick2.bind(this)}>Select</button>
                 </div>
                 <div className="graph-card">
                     Labor Market (perfectly inelastic)
                     <JXGBoard id={'jxg-labor-market-inelastic'} gType={2} />
                     <button
-                         type="button" className="btn btn-default"
+                         type="button" className="btn btn-default btn-sm"
                          onClick={this.handleClick3.bind(this)}>Select</button>
                 </div>
                 <div className="graph-card">
                     Cobb-Douglas
                     <JXGBoard id={'jxg-cobb-douglas'} gType={3} />
                     <button
-                         type="button" className="btn btn-default"
+                         type="button" className="btn btn-default btn-sm"
                          onClick={this.handleClick4.bind(this)}>Select</button>
                 </div>
                 <div className="graph-card">
                     Labor Supply
                     <JXGBoard id={'jxg-labor-supply'} gType={4} />
                     <button
-                         type="button" className="btn btn-default"
+                         type="button" className="btn btn-default btn-sm"
                          onClick={this.handleClick5.bind(this)}>Select</button>
                 </div>
                 <div className="graph-card">
                     Consumption - Saving
                     <JXGBoard id={'jxg-consumption-saving'} gType={5} />
                     <button
-                         type="button" className="btn btn-default"
+                         type="button" className="btn btn-default btn-sm"
                          onClick={this.handleClick6.bind(this)}>Select</button>
                 </div>
                 <div className="graph-card">
                     Saving - Investment
                     <JXGBoard id={'jxg-saving-investment'} gType={6} />
                     <button
-                         type="button" className="btn btn-default"
+                         type="button" className="btn btn-default btn-sm"
                          onClick={this.handleClick7.bind(this)}>Select</button>
                 </div>
                 <div className="graph-card">
                     Money Market
                     <JXGBoard id={'jxg-money-market'} gType={7} />
                     <button
-                         type="button" className="btn btn-default"
+                         type="button" className="btn btn-default btn-sm"
                          onClick={this.handleClick8.bind(this)}>Select</button>
                 </div>
             </div>
