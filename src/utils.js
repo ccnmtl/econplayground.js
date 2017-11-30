4f11ac20e9da742eb2c1521f878d5f371f2e52ca
Nik Nyby <nnyby@columbia.edu>
2017-11-30 15:01:13 -0500

    Add getOrCreateSubmission()

--- src/utils.js
+++ src/utils.js
@@ -1,7 +1,7 @@
 import Cookies from 'js-cookie';
 
 /**
- * A version of `fetch` that passes along auth credentials.
+ * A wrapper for `fetch` that passes along auth credentials.
  */
 let authedFetch = function(url, method = 'get', data = null) {
     const token = Cookies.get('csrftoken');
@@ -18,4 +18,42 @@ let authedFetch = function(url, method = 'get', data = null) {
     });
 };
 
-export { authedFetch };
+/**
+ * Returns a Promise containing the submission for the current user
+ * and given graph id, if it exists.
+ */
+let getSubmission = function(graphId) {
+    return authedFetch(`/api/submissions/${graphId}/`)
+        .then(function(response) {
+            if (response.status === 200) {
+                return response.json();
+            } else {
+                throw 'Not found';
+            }
+        });
+};
+
+let createSubmission = function(data) {
+    return authedFetch('/api/submissions/', 'post', JSON.stringify(data))
+        .then(function(response) {
+            if (response.status === 201) {
+                // TODO: propagate message up to UI
+                return response.json();
+            } else {
+                throw 'Submission not created';
+            }
+        });
+};
+
+let getOrCreateSubmission = function(data) {
+    return authedFetch(`/api/submissions/${data.graph}/`)
+        .then(function(response) {
+            if (response.status === 200) {
+                return response.json();
+            } else {
+                return createSubmission(data);
+            }
+        });
+};
+
+export { authedFetch, getSubmission, createSubmission, getOrCreateSubmission };
