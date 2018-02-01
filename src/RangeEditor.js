d6b0df35d6cd3891caaa8244a20eaef3b640c8fd
Nik Nyby <nnyby@columbia.edu>
2018-02-01 11:59:58 -0500

    Cobb-Douglas graph: hide numerical values on student view

--- src/RangeEditor.js
+++ src/RangeEditor.js
@@ -17,25 +17,28 @@ export default class RangeEditor extends React.Component {
             />
             </div>
 
-            <div className="col-4">
-            <input
-        className="form-control form-control-sm"
-        data-id={this.props.dataId}
-        type="number"
-        onChange={this.props.handler}
-        value={this.props.value}
-        step="0.01"
-        min={this.props.min}
-        max={this.props.max}
-            />
-            </div>
+        {!this.props.hideNumber &&
+         <div className="col-4">
+         <input
+         className="form-control form-control-sm"
+         data-id={this.props.dataId}
+         type="number"
+         onChange={this.props.handler}
+         value={this.props.value}
+         step="0.01"
+         min={this.props.min}
+         max={this.props.max}
+         />
+         </div>
+        }
             </div>;
     }
 }
 
 RangeEditor.defaultProps = {
     min: -5,
-    max: 5
+    max: 5,
+    hideNumber: false
 };
 
 RangeEditor.propTypes = {
@@ -43,5 +46,6 @@ RangeEditor.propTypes = {
     handler: PropTypes.func.isRequired,
     value: PropTypes.number.isRequired,
     min: PropTypes.number,
-    max: PropTypes.number
+    max: PropTypes.number,
+    hideNumber: PropTypes.bool
 };
