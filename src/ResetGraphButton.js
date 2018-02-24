86cbea7b89722bcd2e6101627f8863f6377065d7
Nik Nyby <nnyby@columbia.edu>
2018-02-24 15:39:24 -0500

    Add "Reset Graph" button to consumption-leisure - PMT #113511
    Still TODO: Add to the other graph types as well.

---
+++ src/ResetGraphButton.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+export default class ResetGraphButton extends React.Component {
+    render() {
+        return (
+            <button className="btn btn-default btn-sm"
+                    onClick={this.onClick.bind(this)}>
+                Reset Graph
+            </button>
+        )
+    }
+    onClick(evt) {
+        evt.preventDefault();
+        this.props.updateGraph(this.props.initialState);
+    }
+}
+
+ResetGraphButton.propTypes = {
+    initialState: PropTypes.object.isRequired,
+    updateGraph: PropTypes.func.isRequired
+}
