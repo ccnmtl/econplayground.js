d17c7754be1af01010793eb1bb642486123abfa5
Nik Nyby <nnyby@columbia.edu>
2018-02-27 14:04:33 -0500

    "Reset Graph" button style update
    Making this button darker, thanks to the note from @sdreher.

--- src/ResetGraphButton.js
+++ src/ResetGraphButton.js
@@ -4,7 +4,7 @@ import PropTypes from 'prop-types';
 export default class ResetGraphButton extends React.Component {
     render() {
         return (
-            <button className="btn btn-default btn-sm"
+            <button className="btn btn-secondary btn-sm"
                     onClick={this.onClick.bind(this)}>
                 Reset Graph
             </button>
