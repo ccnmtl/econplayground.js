b3bd32752bbda6c8683665c51b0b9353bb1e3f36
Nik Nyby <nnyby@columbia.edu>
2018-03-08 13:59:42 -0500

    Add some tests to EditableControl

--- src/form-components/EditableControl.test.js
+++ src/form-components/EditableControl.test.js
@@ -15,3 +15,32 @@ it('renders without crashing', () => {
             updateGraph={function() {}} />
     );
 });
+
+it('Displays checkbox for instructors', () => {
+    const el = TestRenderer.create(
+        <EditableControl
+            id="gIntersectionHorizLineLabel"
+            name="Endowment point&apos;s horizontal line label"
+            value="a"
+            valueEditable={true}
+            isInstructor={true}
+            updateGraph={function() {}} />
+    ).root;
+    const checkbox = el.findByProps({className: 'form-check'});
+    expect(checkbox.type).toEqual('div');
+});
+
+it('Hides checkbox for students', () => {
+    const el = TestRenderer.create(
+        <EditableControl
+            id="gIntersectionHorizLineLabel"
+            name="Endowment point&apos;s horizontal line label"
+            value="a"
+            valueEditable={true}
+            isInstructor={false}
+            updateGraph={function() {}} />
+    ).root;
+    expect(() => {
+        el.findByProps({className: 'form-check'});
+    }).toThrow('No instances found with props: {"className":"form-check"}');
+});
