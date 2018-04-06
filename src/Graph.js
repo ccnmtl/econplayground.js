c30999bacf818253ae1c3de54f9f1f6c3727a2f0
Nik Nyby <nnyby@columbia.edu>
2018-04-06 14:25:08 -0400

    AD-AS graph: allow horizontal lines - PMT #114932

--- src/Graph.js
+++ src/Graph.js
@@ -419,7 +419,7 @@ class NonLinearDemandSupplyGraph extends Graph {
         }
 
         const f1 = function(x) {
-            const slope = me.options.gLine1Slope || 1;
+            const slope = me.options.gLine1Slope;
             return (x - 2.5) * slope + 2.5;
         };
 
@@ -852,7 +852,7 @@ class ADASGraph extends Graph {
         }
 
         const f1 = function(x) {
-            const slope = me.options.gLine1Slope || 1;
+            const slope = me.options.gLine1Slope;
             return (x - 2.5) * slope + 2.5;
         };
 
@@ -866,7 +866,7 @@ class ADASGraph extends Graph {
         });
 
         const f2 = function(x) {
-            const slope = me.options.gLine2Slope || -1;
+            const slope = me.options.gLine2Slope;
             return (x - 2.5) * slope + 2.5;
         };
 
@@ -880,7 +880,7 @@ class ADASGraph extends Graph {
         });
 
         const f3 = function(x) {
-            const slope = me.options.gLine3Slope || Infinity;
+            const slope = me.options.gLine3Slope;
             return (x - 2.5) * slope + 2.5;
         };
 
