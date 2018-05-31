6aab8af89e50865164d009ee414db6bebc5444e9
Nik Nyby <nnyby@columbia.edu>
2018-05-31 12:15:21 -0400

    Fix getGraph
    woops - i accidentally broke everything

--- src/Viewer.js
+++ src/Viewer.js
@@ -221,13 +221,16 @@ class Viewer extends Component {
         // Load graph and submission data
         const me = this;
 
-        if (this.state.gNeedsSubmit) {
-            this.getGraph().then(function() {
+
+        this.getGraph().then(function() {
+            if (me.state.gNeedsSubmit) {
                 return me.getSubmission();
-            }).then(function(s) {
-                me.setState({submission: s});
-            });
-        }
+            } else {
+                return null;
+            }
+        }).then(function(s) {
+            me.setState({submission: s});
+        });
 
         // Add graph feedback event handlers
         document.addEventListener('l1up', function() {
