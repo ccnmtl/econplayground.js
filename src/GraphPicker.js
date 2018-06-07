60b6068bfba705136092a7990f05fde5f9ff656a
Nick Buonincontri <nbuonin@gmail.com>
2018-06-07 16:35:40 -0400

    PMT #115463: Use images of graphs rather than live JSX rendered objects

--- src/GraphPicker.js
+++ src/GraphPicker.js
@@ -14,49 +14,31 @@ export default class GraphPicker extends React.Component {
                 <p className="lead text-secondary">Build your own ideas by starting with one of these graphs as a foundation.</p>
                 <div className="card-columns">
                     <div className="card">
-                        <JXGBoard
-                            id={'jxg-demand-supply'}
-                            height={181}
-                            width={340}
-                            gType={0}
-                            locked={true} />
+                        <a
+                            ref={(b1) => { this.b1 = b1;  }}
+                            onClick={this.handleClick1.bind(this)}>
+                            <img className="img-fluid" src="/media/img/linear_demand_supply.png"/>
+                        </a>
                         <div className="card-body">
                             <h5 className="card-title">{displayGraphType(0)}</h5>
                             <p className="card-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed semper aliquam ante at iaculis. Donec in dapibus diam. </p>
-                            <button
-                                ref={(b1) => { this.b1 = b1;  }}
-                                type="button" className="btn btn-primary btn-sm"
-                                onClick={this.handleClick1.bind(this)}>
-                                Select
-                                <svg className="octicon octicon-arrow-right octicon-after" viewBox="0 0 10 16" version="1.1" width="10" height="16" aria-hidden="true"><path fillRule="evenodd" d="M10 8L4 3v3H0v4h4v3z"></path></svg>
-                            </button>
                         </div>
                     </div>
                     <div className="card">
-                        <JXGBoard
-                            id={'jxg-nonlinear-demand-supply'}
-                            height={181}
-                            width={340}
-                            gType={1}
-                            locked={true} />
+                        <a
+                            onClick={this.handleClick2.bind(this)}>
+                            <img className="img-fluid" src="/media/img/non-linear_demand_supply.png"/>
+                        </a>
                         <div className="card-body">
                             <h5 className="card-title">{displayGraphType(1)}</h5>
                             <p className="card-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed semper aliquam ante at iaculis. Donec in dapibus diam. </p>
-                            <button
-                                type="button" className="btn btn-primary btn-sm"
-                                onClick={this.handleClick2.bind(this)}>
-                                Select
-                                <svg className="octicon octicon-arrow-right octicon-after" viewBox="0 0 10 16" version="1.1" width="10" height="16" aria-hidden="true"><path fillRule="evenodd" d="M10 8L4 3v3H0v4h4v3z"></path></svg>
-                            </button>
                         </div>
                     </div>
                     <div className="card">
-                        <JXGBoard
-                            id={'jxg-cobb-douglas'}
-                            height={181}
-                            width={340}
-                            gType={3}
-                            locked={true} />
+                        <a
+                            onClick={this.handleClick4.bind(this)}>
+                            <img className="img-fluid" src="/media/img/cobb_douglas.png"/>
+                        </a>
                         <div className="card-body">
                             <h5 className="card-title">{displayGraphType(3)}</h5>
                             <p className="card-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed semper aliquam ante at iaculis. Donec in dapibus diam. </p>
@@ -69,57 +51,33 @@ export default class GraphPicker extends React.Component {
                         </div>
                     </div>
                     <div className="card">
-                        <JXGBoard
-                            id={'jxg-cons-leisure-choice'}
-                            height={181}
-                            width={340}
-                            gType={5}
-                            locked={true} />
+                        <a
+                            onClick={this.handleClick6.bind(this)}>
+                            <img className="img-fluid" src="/media/img/consumption_leisure.png"/>
+                        </a>
                         <div className="card-body">
                             <h5 className="card-title">{displayGraphType(5)}</h5>
                             <p className="card-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed semper aliquam ante at iaculis. Donec in dapibus diam. </p>
-                            <button
-                                type="button" className="btn btn-primary btn-sm"
-                                onClick={this.handleClick6.bind(this)}>
-                                Select
-                                <svg className="octicon octicon-arrow-right octicon-after" viewBox="0 0 10 16" version="1.1" width="10" height="16" aria-hidden="true"><path fillRule="evenodd" d="M10 8L4 3v3H0v4h4v3z"></path></svg>
-                            </button>
                         </div>
                     </div>
                     <div className="card">
-                        <JXGBoard
-                            id={'jxg-cons-saving-choice'}
-                            height={181}
-                            width={340}
-                            gType={7}
-                            locked={true} />
+                        <a
+                            onClick={this.handleClick8.bind(this)}>
+                            <img className="img-fluid" src="/media/img/consumption_saving.png"/>
+                        </a>
                         <div className="card-body">
                             <h5 className="card-title">{displayGraphType(7)}</h5>
                             <p className="card-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed semper aliquam ante at iaculis. Donec in dapibus diam. </p>
-                            <button
-                                type="button" className="btn btn-primary btn-sm"
-                                onClick={this.handleClick8.bind(this)}>
-                                Select
-                                <svg className="octicon octicon-arrow-right octicon-after" viewBox="0 0 10 16" version="1.1" width="10" height="16" aria-hidden="true"><path fillRule="evenodd" d="M10 8L4 3v3H0v4h4v3z"></path></svg>
-                            </button>
                         </div>
                     </div>
                     <div className="card">
-                        <JXGBoard
-                            id={'jxg-adas-choice'}
-                            height={181}
-                            width={340}
-                            gType={8}
-                            locked={true} />
+                        <a
+                            onClick={this.handleClick9.bind(this)}>
+                            <img className="img-fluid" src="/media/img/ADAS.png"/>
+                        </a>
                         <div className="card-body">
                             <h5 className="card-title">{displayGraphType(8)}</h5>
                             <p className="card-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed semper aliquam ante at iaculis. Donec in dapibus diam. </p>
-                            <button
-                                type="button" className="btn btn-primary btn-sm"
-                                onClick={this.handleClick9.bind(this)}>
-                                Select
-                                <svg className="octicon octicon-arrow-right octicon-after" viewBox="0 0 10 16" version="1.1" width="10" height="16" aria-hidden="true"><path fillRule="evenodd" d="M10 8L4 3v3H0v4h4v3z"></path></svg>
-                            </button>
                         </div>
                     </div>
                 </div>
