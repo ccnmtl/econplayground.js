7c960efb0683e479555f5dea4d897805dfd76e34
Nikolas Nyby <nikolas@gnu.org>
2019-02-01 11:32:15 -0500

    Set text display to internal - close PMT #115040

--- src/JXGBoard.js
+++ src/JXGBoard.js
@@ -88,6 +88,9 @@ export default class JXGBoard extends React.Component {
                 break;
         }
 
+        // https://jsxgraph.uni-bayreuth.de/wp/2012/11/02/howto-export-jsxgraph-constructions/
+        JXG.Options.text.display = 'internal';
+
         let board = JXG.JSXGraph.initBoard(
             this.id, {
                 axis: true,
