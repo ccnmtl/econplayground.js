5d3d6c6f2bcefa1a2314ba8b3acd69ce18729223
Nikolas Nyby <nikolas@gnu.org>
2020-10-19 12:16:53 -0400

    Fix webpack 5 errors

--- config/webpack.config.prod.js
+++ config/webpack.config.prod.js
@@ -146,14 +146,5 @@ module.exports = {
         // https://github.com/jmblog/how-to-optimize-momentjs-with-webpack
         // You can remove this if you don't use Moment.js:
         new webpack.IgnorePlugin(/^\.\/locale$/, /moment$/),
-    ],
-    // Some libraries import Node modules but don't use them in the browser.
-    // Tell Webpack to provide empty mocks for them so importing them works.
-    node: {
-        dgram: 'empty',
-        fs: 'empty',
-        net: 'empty',
-        tls: 'empty',
-        child_process: 'empty',
-    },
+    ]
 };
