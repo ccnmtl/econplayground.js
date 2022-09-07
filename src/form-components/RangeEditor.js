caab39a23d6860368968c280512fbddf8cb7db98
Evan-CTL <emp2221@columbia.edu>
2022-09-07 15:55:39 -0400

    Range editor partial edits -- might conflict with template

--- src/form-components/RangeEditor.js
+++ src/form-components/RangeEditor.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { isNumber } from 'lodash';
 
 /**
  * RangeEditor is a re-usable component that creates an <input> with
@@ -9,64 +10,125 @@ import PropTypes from 'prop-types';
 export default class RangeEditor extends React.Component {
     render() {
         return <React.Fragment>
-            <div className="d-flex justify-content-between">
-            {this.props.showMinMax && (
-                <div className="ep-min-max">
-                    {this.props.min}
-                </div>
-            )}
-            <input
-                className="form-control-range form-control-sm"
-                id={this.props.id}
-                data-id={this.props.dataId}
-                type="range"
-                onChange={this.props.handler}
-                value={this.props.value}
-                step={this.props.step || 0.01}
-                min={this.props.min}
-                max={this.props.max}
-            />
-                {this.props.showMinMax && (
-                    <div className="ep-min-max ep-max">
-                        {this.props.max}
-                    </div>
-                )}
-        </div>
-            <div className="input-group">
-            {this.props.showOverrideButton && (
-                <div className="form-check form-check-inline">
-                    <label className="form-check-label">
+            <div className="form-row slider-wrapper">
+                <div className="d-inline">
+                    <div className="d-inline">
+                        {this.props.showMinMax && (
+                            <div className="d-inline">
+                                {this.props.min}
+                            </div>
+                        )}
+                        <label htmlFor={this.props.id}>
+                            
+                            <em>{this.props.itemlabel}</em>
+                            <input
+                                className="d-inline form-control-range form-control-sm"
+                                id={this.props.id}
+                                data-id={this.props.dataId}
+                                type="range"
+                                onChange={this.props.handler}
+                                value={this.props.value}
+                                step={this.props.step || 0.01}
+                                min={this.props.min}
+                                max={this.props.max}
+                            />
+                        </label>
+                        {this.props.showMinMax && (
+                            <div className="d-inline">
+                                {this.props.max}
+                            </div>
+                        )}
                         <input
+                            className=""
                             data-id={this.props.dataId}
-                            data-override={this.props.overrideValue}
-                            className="form-check-input override"
-                            type="radio"
+                            type="number"
                             onChange={this.props.handler}
-                            checked={this.props.value === this.props.overrideValue} />
-                        {this.props.overrideLabel}
-                    </label>
+                            value={this.props.value}
+                            step={this.props.step || 0.01}
+                            min={this.props.min}
+                            max={this.props.max}
+                        />
+                        <button
+                            className="btn btn-primary ml-2"
+                            id={this.props.id}
+                            data-id={this.props.dataId}
+                            type="button"
+                            onClick={this.props.handler}
+                            value={this.btnStep(-1, Number(this.props.step) * 10 || 0.1)}
+                        >&lt;&lt;&lt;</button>
+                        <button
+                            className="btn btn-primary ml-2"
+                            id={this.props.id}
+                            data-id={this.props.dataId}
+                            type="button"
+                            onClick={this.props.handler}
+                            value={this.btnStep(-1, Number(this.props.step) || 0.01)}
+                        >&lt;</button>
+                        <button
+                            className="btn btn-primary ml-2"
+                            id={this.props.id}
+                            data-id={this.props.dataId}
+                            type="button"
+                            onClick={this.props.handler}
+                            value={this.btnStep(1, Number(this.props.step) || 0.01)}
+                        >&gt;</button>
+                        <button
+                            className="btn btn-primary ml-2"
+                            id={this.props.id}
+                            data-id={this.props.dataId}
+                            type="button"
+                            onClick={this.props.handler}
+                            value={this.btnStep(1, Number(this.props.step) * 10 || 0.1)}
+                        >&gt;&gt;&gt;</button>
+                    </div>
                 </div>
-            )}
-        {this.props.showOverride2Button && (
-            <div className="form-check form-check-inline">
-                <label className="form-check-label">
-                    <input
-                        data-id={this.props.dataId}
-                        data-override={this.props.override2Value}
-                        className="form-check-input override"
-                        type="radio"
-                        onChange={this.props.handler}
-                        checked={this.props.value === this.props.override2Value} />
-                    {this.props.override2Label}
-                </label>
+                <div className="input-group">
+                    {this.props.showOverrideButton && (
+                        <div className="form-check form-check-inline">
+                            <label className="form-check-label">
+                                <input
+                                    data-id={this.props.dataId}
+                                    data-override={this.props.overrideValue}
+                                    className="form-check-input override"
+                                    type="radio"
+                                    onChange={this.props.handler}
+                                    checked={this.props.value === this.props.overrideValue} />
+                                {this.props.overrideLabel}
+                            </label>
+                        </div>
+                    )}
+                    {this.props.showOverride2Button && (
+                        <div className="form-check form-check-inline">
+                            <label className="form-check-label">
+                                <input
+                                    data-id={this.props.dataId}
+                                    data-override={this.props.override2Value}
+                                    className="form-check-input override"
+                                    type="radio"
+                                    onChange={this.props.handler}
+                                    checked={this.props.value === this.props.override2Value} />
+                                {this.props.override2Label}
+                            </label>
+                        </div>
+                    )}
+                </div>
             </div>
-        )}
-        </div>
         </React.Fragment>;
+    }
+    btnStep(sign, strength) {
+        let min = Number(this.props.min);
+        min = !isNaN(min) ? min : -999;
+        let max = Number(this.props.max);
+        max = !isNaN(max) ? max : 999;
+        let val = Number(this.props.value) + (sign * strength);
+        val = val < min ? min : val;
+        val = val > max ? max : val;
+        return Number(val);
     }
 }
 
 RangeEditor.defaultProps = {
+    itemlabel: '',
     min: -5,
     max: 5,
     showOverrideButton: false,
@@ -75,7 +137,7 @@ RangeEditor.defaultProps = {
     showOverride2Button: false,
     override2Label: '',
     override2Value: 0,
-    showMinMax: false
+    showMinMax: true
 };
 
 RangeEditor.propTypes = {
@@ -87,6 +149,7 @@ RangeEditor.propTypes = {
     min: PropTypes.number,
     max: PropTypes.number,
     showOverrideButton: PropTypes.bool,
+    itemlabel: PropTypes.string,
     overrideLabel: PropTypes.string,
     overrideValue: PropTypes.number,
     showOverride2Button: PropTypes.bool,
